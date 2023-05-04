c724e7ac44d9571448d3140cd416ea5b17b0f401
Aadityakiran S <aadityakirans@gmail.com>
2023-05-04 09:04:50 +0530

    Wrote all lambda functions

--- lambda-authorizer/handler/app.js
+++ lambda-authorizer/handler/app.js
@@ -1,18 +1,215 @@
+const AWS = require("aws-sdk");
+const ENTRY_DB = process.env.ENTRY_DB;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
 
-let response;
+exports.listAllEntries = async (event, context) => {
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
 
-exports.lambdaHandler = async (event, context) => {
+    let body; let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        TableName: ENTRY_DB,
+    };
     try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
+        body = await dynamoDb.scan(params).promise();
+    } catch (error) {
+        statusCode = 400;
+        body = error.message;
+        console.log(error);
+    } finally {
+        body = JSON.stringify(body);
     }
 
-    return response
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
+
+exports.createEntry = async (event, context) => {
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+
+    let body = {}; let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        TableName: ENTRY_DB,
+        Item: {
+            id: `${uuid.v1()}`,
+            name: data.name,
+            createdAt: timestamp,
+            updatedAt: timestamp,
+        },
+    };
+
+    try {
+        body = await dynamoDb.put((params)).promise();
+        body.message = `Successfully created entry with name ${data.name}`;
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+        console.log(err);
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers
+    };
+};
+
+exports.getEntry = async (event, context) => {
+    let body = {}; let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        TableName: ENTRY_DB,
+        Key: {
+            id: `${event.pathParameters.id}`
+        }
+    };
+
+    try {
+        body = await dynamoDb.get(params).promise();
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+        console.log(err);
+    } finally {
+        //A log to see if item with given key exists
+        if (body.Item == undefined || body.Item == null) {
+            body.message = `Item with id ${event.pathParameters.id} DNE`;
+        }
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
+
+exports.updateEntry = async (event, context) => {
+    const timestamp = new Date().getTime();
+    let body = {};
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    //#region  Error check to see if item with given ID exists
+    const getItemParams = {
+        TableName: ENTRY_DB,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+
+    try {
+        const getItemResult = await dynamoDb.get(getItemParams).promise();
+        if (getItemResult.Item == undefined || getItemResult.Item == null) {
+            statusCode = 404;
+            body.message = `Item with id ${event.pathParameters.id} not found`;
+            return {
+                statusCode,
+                body: JSON.stringify(body),
+                headers,
+            };
+        }
+    } catch (err) {
+        statusCode = 400;
+        body.message = `Unable to update item with id ${event.pathParameters.id}: ${err.message}`;
+        console.log(err);
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+            headers,
+        };
+    }
+    //#endregion
+
+    // Update the item
+    const updateItemParams = {
+        TableName: ENTRY_DB,
+        Key: {
+            id: event.pathParameters.id
+        },
+        UpdateExpression: "SET #name = :name, #updatedAt = :updatedAt",
+        ExpressionAttributeNames: {
+            "#name": "name"
+        },
+        ExpressionAttributeValues: {
+            ":name": event.body.name,
+            ":updatedAt": timestamp
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    try {
+        body = await dynamoDb.update(updateItemParams).promise();
+        body.message = `Item with id ${event.pathParameters.id} updated successfully`;
+    } catch (err) {
+        statusCode = 400;
+        body.message = `Unable to update item with id ${event.pathParameters.id}: ${err.message}`;
+        console.log(err);
+    } finally {
+        body = JSON.stringify(body.Attributes);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
+
+exports.deleteEntry = async (event, context) => {
+    let body = {};
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        TableName: ENTRY_DB,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+
+    try {
+        await dynamoDb.delete(params).promise();
+    } catch (err) {
+        statusCode = 400;
+        body.message = `Unable to delete item with id ${event.pathParameters.id}: ${err.message}`;
+        console.log(err);
+    } finally {
+        if (body.Item == undefined || body.Item == null) {
+            body.message = `Item with id ${event.pathParameters.id} DNE`;
+        }
+        else {
+            body.message = `Item with id ${event.pathParameters.id} deleted successfully`;
+        }
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
 };
